0b8c8033507e1e8c5e1b9609f92548ea1b5134b0
Harry Chiu <harrychiu1106@gmail.com>
2020-05-20 17:11:23 +0800

    Updates

---
+++ precache-manifest.18bb43a00efacb8b6fe27d6d6e5769c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37a470d19c857be041dcedc2e69d451",
+    "url": "/react-lottery/index.html"
+  },
+  {
+    "revision": "ef7855a94153fcbd6469",
+    "url": "/react-lottery/static/css/main.c6ecebfc.chunk.css"
+  },
+  {
+    "revision": "f4d1f24cbd1f224fab2e",
+    "url": "/react-lottery/static/js/2.0a19f37a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-lottery/static/js/2.0a19f37a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7855a94153fcbd6469",
+    "url": "/react-lottery/static/js/main.3bf5d9d0.chunk.js"
+  },
+  {
+    "revision": "abd4665fb782f510f0b5",
+    "url": "/react-lottery/static/js/runtime-main.7015cfc0.js"
+  }
+]);
